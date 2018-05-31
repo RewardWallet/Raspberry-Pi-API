ce9163974f9f377723d50fca3354b03bb8292d61
Nathan Tannar <nathantannar4@gmail.com>
2018-05-31 13:42:25 -0700

    2018-05-31

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ An API that runs on the Raspberry Pi to allow for configuration changes
 ## Installation
 
 ```
-pip install -r requirements.text // python 2
-pip3 install -r requirements.text // python 3
+pip install -r requirements.txt // python 2
+pip3 install -r requirements.txt // python 3
 ```
 
 ## Running
 
 ```
+. venv/bin/activate // For venv
 FLASK_APP=app.py flask run
 ```
--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 
-from flask import Flask, session, redirect, url_for, escape, request
+from flask import Flask, session, redirect, url_for, escape, request # Flask
+import json, requests, urllib # Parse Server
 
 app = Flask(__name__)
+
+API_ROOT = 'https://nathantannar.me/api/dev'
+APP_ID = '5++ejBLY/kzVaVibHAIIQZvbawrEywUCNqpD+FVpHgU='
+APP_KEY = 'oR3Jp5YMyxSBu6r6nh9xuYQD5AcsdubQmvATY1OEtXo='
+API_HEADERS = {
+    "X-Parse-Application-ID": APP_ID,
+    "X-Parse-Master-Key": APP_KEY,
+    "Content-Type": "application/json"
+}
 
 @app.route('/')
 def index():
@@ -12,8 +22,19 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        session['username'] = request.form['username']
-        session['password'] = request.form['password']
+        username = request.form['username']
+        password = request.form['password']
+        session['username'] = username
+        params = urllib.parse.urlencode({"username": username, "password": password})
+        url = API_ROOT + ('/login?%s' % params)
+        headers = {
+            "X-Parse-Application-Id": APP_ID,
+            "X-Parse-Master-Key": APP_KEY,
+            "X-Parse-Revocable-Session": "1"
+        }
+        response = requests.get(url, headers=headers, verify=False)
+        json_data = json.loads(response.text)
+        print(json_data)
         return redirect(url_for('index'))
     return '''
         <form method="post">
@@ -27,7 +48,6 @@ def login():
 def signup():
     if request.method == 'POST':
         session['username'] = request.form['username']
-        session['password'] = request.form['password']
         return redirect(url_for('index'))
     return '''
         <form method="post">
@@ -41,7 +61,6 @@ def signup():
 def logout():
     # remove the username from the session if it's there
     session.pop('username', None)
-    session.pop('password', None)
     return redirect(url_for('index'))
 
 # Config
